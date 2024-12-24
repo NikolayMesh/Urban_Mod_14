4107df0aab393ca130f4d226ceebadb7ebf43735
NikolayMesh <partizan.91@bk.ru>
2024-12-24 22:43:08 +0300

    add

---
+++ module_14_2.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+""")
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users(email)')
+cursor.execute("DELETE FROM Users;") # удаление данных из таблицы(для проверки УДАЛИТЬ!!)
+for i in range(1,11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)', (f"User{i}",f"example{i}@gmail.com",f"{i}0", "1000"))
+cursor.execute("UPDATE Users SET balance = balance - 500 WHERE id % 2 = 1") # Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1") # Удалите каждую 3ую запись в таблице начиная с 1ой:
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")#
+results = cursor.fetchall()
+for row in results:
+    username, email, age, balance = row
+    # print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}')
+cursor.execute("DELETE FROM Users WHERE id = 6" ) # Удаление пользователя с id=6
+
+cursor.execute("SELECT COUNT(*) FROM Users") # Подсчёт кол-ва всех пользователей
+total_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users ') # Подсчёт суммы всех балансов
+all_balances = cursor.fetchone()[0]
+
+print(all_balances / total_users)
+
+connection.commit()
+connection.close()
